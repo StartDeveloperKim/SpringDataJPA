f953f18239568683c1095d6134a3d2918c1d60c2
StartDeveloperKim <ktw550111@naver.com>
2022-10-12 11:26:04 +0900

    스프링데이터JPA 공부 시작

---
+++ study/src/test/java/springdatajpa/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springdatajpa.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
